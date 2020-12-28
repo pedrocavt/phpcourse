a151d63116ee5753cf548afe76fb6aef81d8bb73
Pedro Vitor <pedrocavt@gmail.com>
2020-12-28 16:58:09 -0300

    2- Conceitos Basicos

---
+++ 2-ConceitosBásicos/1-CaseSensitive/index.php
@@ -0,0 +1,14 @@
+<?php
+ //não é case sensitive
+ echo "Teste <br>";
+ eCho "testando2<br>";
+ ECHO "TESTADO3";
+ 
+ //é case sensitive
+ $name = "Pedro";
+ $NAME = "VITOR";
+
+ echo $name;
+ echo "<br>";
+ echo $NAME;
+?>
---
+++ 2-ConceitosBásicos/2-InstrucaoDeCodigo/index.php
@@ -0,0 +1,9 @@
+<?php
+
+    echo "Uma linha <br>";
+    echo "Outra linha <br>";
+
+    if(5 > 2){
+        echo "Dentro do if";
+    }
+?>
