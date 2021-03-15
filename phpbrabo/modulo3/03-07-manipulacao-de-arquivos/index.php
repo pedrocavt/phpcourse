4d004b2b23d9af6fb88fbf39c282bcb3c9b65e85
Pedro Vitor <pedrocavt@gmail.com>
2021-03-15 11:24:31 -0300

    manipulação de arquivos

--- phpbrabo/modulo3/03-07-manipulacao-de-arquivos/index.php
+++ phpbrabo/modulo3/03-07-manipulacao-de-arquivos/index.php
@@ -7,14 +7,54 @@ fullStackPHPClassName("03.07 - Manipulação de arquivos");
  */
 fullStackPHPClassSession("verificação", __LINE__);
 
+$file = __DIR__ . "/file.txt";
+
+if(file_existS($file) && is_file($file)){
+    echo "<p>Existe</p>";
+}else{
+    echo "<p>NÃO Existe</p>";
+}
 
 /*
  * [ leitura e gravação ] fopen | fwrite | fclose | file
  */
 fullStackPHPClassSession("leitura e gravação", __LINE__);
 
+if(!file_exists($file) || !is_file($file)){
+    $fileOpen = fopen($file, "w");
+    fwrite($fileOpen, "Linha 01" . PHP_EOL);
+    fwrite($fileOpen, "Linha 02" . PHP_EOL);
+    fwrite($fileOpen, "Linha 03" . PHP_EOL);
+    fwrite($fileOpen, "Ronaldo, brilha muito no corinthias" . PHP_EOL);
+    fclose($fileOpen); 
 
+}else{
+    var_dump(
+        file($file),
+        pathinfo($file),
+    );
+}
+
+echo file($file)[3];
+
+$open = fopen($file, "r");
+while(!feof($open)){
+    echo "<p>" . fgets($open) . "</p>";
+}
+fclose($open);
 /*
  * [ get, put content ] file_get_contents | file_put_contents
  */
 fullStackPHPClassSession("get, put content", __LINE__);
+
+$getContents = __DIR__ . "/teste2.txt";
+if(file_exists($getContents) && is_file($getContents)){
+    echo file_get_contents($getContents);
+}else{
+    $data = "<article><h1>Robson</h1><p> CEO UpInside<br>Cursosgmail.com</p></article>";
+    file_put_contents($getContents,$data);
+}
+
+
+unlink($getContents);
+unlink($file);
