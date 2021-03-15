4a1fe6a6fd894aea089759a08d2b58977345e5af
Pedro Vitor <pedrocavt@gmail.com>
2021-03-15 10:34:31 -0300

    Class DateTime

--- phpbrabo/modulo3/03-06-uma-classe-datetime/index.php
+++ phpbrabo/modulo3/03-06-uma-classe-datetime/index.php
@@ -7,6 +7,31 @@ fullStackPHPClassName("03.06 - Uma classe DateTime");
  */
 fullStackPHPClassSession("A classe DateTime", __LINE__);
 
+define("DATE_BR", "d/m/Y H:i:s");
+
+$dateNow = new DateTime();
+$dateBirth = new DateTime("1995-10-11");
+$dateStatic = DateTime::createFromFormat("d/m/Y", "10/11/1995");
+
+var_dump(
+    $dateNow,
+    $dateBirth,
+    $dateStatic
+);
+
+var_dump(
+    $dateNow->format(DATE_BR),
+    $dateNow->format("d")
+);
+
+$newTimeZone = new DateTimeZone("Pacific/Apia");
+$newDateTime = new DateTime("now", $newTimeZone);
+
+var_dump(
+    $newTimeZone,
+    $newDateTime,
+    $dateNow
+);
 
 /*
  * [ DateInterval ] http://php.net/manual/en/class.dateinterval.php
@@ -14,8 +39,52 @@ fullStackPHPClassSession("A classe DateTime", __LINE__);
  */
 fullStackPHPClassSession("A classe DateInterval", __LINE__);
 
+$dateInterval = new DateInterval("P10Y2MT10M");
+var_dump($dateInterval);
+
+$dateTime = new DateTime("now");
+$dateTime->add($dateInterval);
+var_dump($dateTime);
+
+$birth = new DateTime(date("Y") . "-11-10");
+$dateNow = new DateTime("now");
+
+$diff = $dateNow->diff($birth);
+
+var_dump($birth, $diff);
+
+
+if($diff->invert){
+    echo "seu aniversário foi a {$diff->days}";
+}else{
+    echo "Faltam {$diff->days} para seu aniversário";
+}
+
+$dateResources = new DateTime("now");
+var_dump([
+    $dateResources->format(DATE_BR),
+    $dateResources->sub(DateInterval::createFromDateString("10days"))->format(DATE_BR),
+]);
+
 
 /**
  * [ DatePeriod ] http://php.net/manual/pt_BR/class.dateperiod.php
  */
 fullStackPHPClassSession("A classe DatePeriod", __LINE__);
+
+$start = new DateTime("now");
+$interval = new DateInterval("P1M");
+$end = new DateTime("2022-01-01");
+
+$period = new DatePeriod($start, $interval, $end);
+
+var_dump([
+    $start->format(DATE_BR),
+    $interval,
+    $end->format(DATE_BR)
+], $period, get_class_methods($period));
+
+echo "<h1>Sua assinatura:</h1>";
+foreach($period as $recurrences){
+    echo "<p>Proximo vencimento {$recurrences->format(DATE_BR)}</p>";
+}
