4841a3c4854f7c6566aca22353f192c1fc738a7b
Pedro Vitor <pedrocavt@gmail.com>
2021-03-15 12:11:27 -0300

    Manipulação de diretórios

--- phpbrabo/modulo3/03-08-gestao-de-diretorios/index.php
+++ phpbrabo/modulo3/03-08-gestao-de-diretorios/index.php
@@ -8,13 +8,35 @@ fullStackPHPClassName("03.08 - Gestão de diretórios");
 fullStackPHPClassSession("verificar, criar e abrir", __LINE__);
 
 
+$folder = __DIR__ . "/uploads";
+if(!file_exists($folder) || !is_dir($folder)){
+    mkdir($folder, 0775);
+}else{
+    var_dump(scandir($folder));
+}
+
 /*
  * [ copiar e renomear ] copy | rename
  */
 fullStackPHPClassSession("copiar e renomear", __LINE__);
 
+$file = __DIR__ . "/file.txt";
 
+var_dump(
+    pathinfo($file),
+    scandir($folder),
+    scandir(__DIR__)
+);
+
+if(!file_exists($file) || !is_file($file)){
+    fopen($file, "w");
+}else{
+    // copy($file, $folder . "/" . basename($file));
+    rename($file, __DIR__. "uploads" . time() . "." . pathinfo($file)["extension"]);
+}
 /*
  * [ remover e deletar ] unlink | rmdir
  */
 fullStackPHPClassSession("remover e deletar", __LINE__);
+
+rmdir(__DIR__ . "/remove");
