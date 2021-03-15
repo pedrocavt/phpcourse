7256f046e3d3d2138988b6609e2403e133f4fe2f
Pedro Vitor <pedrocavt@gmail.com>
2021-03-15 18:38:28 -0300

    upload de arquivos

--- phpbrabo/modulo3/03-10-upload-de-arquivos/index.php
+++ phpbrabo/modulo3/03-10-upload-de-arquivos/index.php
@@ -7,3 +7,44 @@ fullStackPHPClassName("03.10 - Upload de arquivos");
  * [ upload errors ] http://php.net/manual/pt_BR/features.file-upload.errors.php
  */
 fullStackPHPClassSession("upload", __LINE__);
+
+$folder = __DIR__ . "/uploads";
+if(!file_exists($folder) || !is_dir($folder)){
+    mkdir($folder, 0755);
+}
+
+include __DIR__ . "/form.php";
+
+$getPost = filter_input(INPUT_GET, "post", FILTER_VALIDATE_BOOLEAN);
+
+if($_FILES && !empty($_FILES['file']['name'])){
+    $fileUpload = $_FILES['file'];
+    var_dump($fileUpload);
+
+    $allowebTypes = [
+        "image/jpg",
+        "image/jpeg",
+        "image/png",
+        "application/pdf"
+    ];
+
+    $newFileName = time() . mb_strstr($fileUpload['name'], ".");
+
+    if(in_array($fileUpload['type'], $allowebTypes)){
+        if(move_uploaded_file($fileUpload['tmp_name'], __DIR__."/uploads/{$newFileName}")){
+            echo "<p class='trigger accept'>Arquivo enviado!</p>";
+        }else{
+            echo "<p class='trigger error'>Erro inesperado!</p>";
+        }
+    }else{
+        echo "<p class='trigger error'>Tipo de arquivo não permitido!</p>";
+    }
+
+}elseif($getPost){
+    echo "<p class='trigger warning'>Whoops, pareque que o arquivo é muito grande</p>";
+}
+else{
+    if($_FILES){
+        echo "<p class='trigger warning'>Selecione um arquivo antes de enviar!</p>";
+    }
+}
