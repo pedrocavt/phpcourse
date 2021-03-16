8c43f07af6d6723be7c392a041c0f617b49d03b6
Pedro Vitor <pedrocavt@gmail.com>
2021-03-16 15:50:16 -0300

    qualificação e encapsulamento

--- phpbrabo/modulo4/04-03-qualificacao-e-encapsulamento/index.php
+++ phpbrabo/modulo4/04-03-qualificacao-e-encapsulamento/index.php
@@ -7,14 +7,56 @@ fullStackPHPClassName("04.03 - Qualificação e encapsulamento");
  */
 fullStackPHPClassSession("namespaces", __LINE__);
 
+require __DIR__."/classes/App/Template.php";
+require __DIR__."/classes/Web/Template.php";
+
+$appTemplate = new App\Template();
+$webTemplate = new Web\Template();
+
+var_dump(
+    $appTemplate,
+    $webTemplate
+);
+
+use App\Template;
+use Web\Template AS WebTemplate;
+
+$appUseTemplate = new Template();
+$webUseTemplate = new Template();
+
+var_dump(
+    $appUseTemplate,
+    $webUseTemplate
+);
+
 
 /*
  * [ visibilidade ] http://php.net/manual/pt_BR/language.oop5.visibility.php
  */
 fullStackPHPClassSession("visibilidade", __LINE__);
 
+require __DIR__."/source/Qualifield/User.php";
+
+$user = new \Source\Qualifield\User();
+
+// $user->setFirstName("Pedro");
+// $user->setLastName("Silva");
+
+var_dump($user);
 
 /*
  * [ manipulação ] Classes com estruturas que abstraem a rotina de manipulação de objetos
  */
 fullStackPHPClassSession("manipulação", __LINE__);
+
+$robson = $user->setUser(
+    "Pedro",
+    "Silva",
+    "pedro@gmail.com"
+);
+
+if(!$robson){
+    echo "<p class='trigger error'>{$user->getError()}</p>";
+}
+
+var_dump($user);
