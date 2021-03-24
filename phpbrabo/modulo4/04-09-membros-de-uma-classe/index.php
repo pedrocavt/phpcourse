e49c152e1a8cf6da0afada8abdc09a0671f131e1
Pedro Vitor <pedrocavt@gmail.com>
2021-03-24 11:50:03 -0300

    Membros de uma classe

--- phpbrabo/modulo4/04-09-membros-de-uma-classe/index.php
+++ phpbrabo/modulo4/04-09-membros-de-uma-classe/index.php
@@ -9,20 +9,53 @@ require __DIR__ . "/source/autoload.php";
  */
 fullStackPHPClassSession("constantes", __LINE__);
 
+use \Source\Members\Config;
+$config = new Config();
+echo "<p>" . $config::COMPANY . "</p>";
+
+var_dump(Config::COMPANY,
+// Config::DOMAIN
+);
+
+$reflection = new ReflectionClass(Config::class);
+
+var_dump($config, $reflection->getConstants());
 
 /*
  * [ propriedades ] http://php.net/manual/pt_BR/language.oop5.static.php
  */
 fullStackPHPClassSession("propriedades", __LINE__);
 
+Config::$company = "UpInside";
+Config::$domain = "UpInside.com.br";
+
+$config::$sector = "Tecnologia";
+var_dump($config, $reflection->getMethods(),$reflection->getDefaultProperties());
 
 /*
  * [ métodos ] http://php.net/manual/pt_BR/language.oop5.static.php
  */
 fullStackPHPClassSession("métodos", __LINE__);
 
+$config::setConfig("", "", "");
+Config::setConfig("UpInside", "upsindide.com.br", "educação");
+
+var_dump($config, $reflection->getMethods(),$reflection->getDefaultProperties());
 
 /*
  * [ exemplo ] Uma classe trigger
  */
 fullStackPHPClassSession("exemplo", __LINE__);
+
+use Source\Members\Trigger;
+
+$trigger = new Trigger();
+$trigger::show("Um objeto trigger");
+
+Trigger::show("Essa é uma mensagem");
+Trigger::show("Essa é uma mensagem", Trigger::ACCEPT);
+Trigger::show("Essa é uma mensagem", Trigger::WARNING);
+
+echo Trigger::push("Essa é um retorno para o usuário");
+
+
